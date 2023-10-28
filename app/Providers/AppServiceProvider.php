796dea4f55be7415360585492923b9763dcfe123
Elanrif <saidbacoelanrif@gmail.com>
2023-10-29 00:11:12 +0100

    second commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        /* pour éviter l'erreur : Syntax error or access violation: 1071 La clé est trop longue. Longueur maximale: 1000 (Connection: mysql, SQL: alter table `password_reset_tokens` add primary key (`email`)) */
+
+        /* Link : https://stackoverflow.com/questions/63015038/laravel-error-1071-key-was-too-long-php-artisan-migrate */
+        Schema::defaultStringLength(191);
     }
 }
---
+++ resources/views/products/layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Laravel 10 CRUD Application - ItsolutionStuff.com</title>
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+</html>
